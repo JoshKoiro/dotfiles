0394f6dff81127674f396d415b493b23e7262047
JoshKoiro <josh.koiro@gmail.com>
2024-02-02 14:32:43 -0500

    added gruvbox color scheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = function()
+      return {
+        transparent_mode = true,
+      }
+    end,
+  },
+  {
     "craftzdog/solarized-osaka.nvim",
     lazy = false,
     priority = 1000,
